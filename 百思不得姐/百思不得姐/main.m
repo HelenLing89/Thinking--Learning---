36e2bbe957b2ff23b33c941c870630c7c22c14a9
Kevin <zhuhaoyang@sunallies.com>
2017-12-18 11:09:50 +0800

    初始化文件

---
+++ 百思不得姐/百思不得姐/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  百思不得姐
+//
+//  Created by KevinMitnick on 2017/12/18.
+//  Copyright © 2017年 helen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

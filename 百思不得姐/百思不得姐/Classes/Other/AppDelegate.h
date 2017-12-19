c4b7ada353ff5c19b27f479e2420ba166eee4fdc
Kevin <zhuhaoyang@sunallies.com>
2017-12-19 15:03:00 +0800

    添加AppDelegate+main文件

---
+++ 百思不得姐/百思不得姐/Classes/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思不得姐
+//
+//  Created by KevinMitnick on 2017/12/18.
+//  Copyright © 2017年 helen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
